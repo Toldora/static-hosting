e9fc7ea918445be1866854171fe64f720236f555
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-04-10 11:41:28 +0300

    static hosting

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const casinosRoutes = require('./routes/casinos');
 
 const PORT = process.env.PORT || 3030;
+const HOST = process.env.HOST || '127.0.0.1';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', casinosRoutes);
 
-async function init() {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://kropivnyi:toldora21@cluster0.qppwslg.mongodb.net/casinos',
-      {
-        useNewUrlParser: true,
-      },
-    );
-    app.listen(PORT, () => {
-      console.log('Server has been started...');
-    });
-  } catch (e) {
-    console.log(e);
-  }
+app.use(express.static(`${__dirname}/assets`));
+
+function init() {
+  app.listen(PORT, HOST, function () {
+    console.log(`Server listens http://${HOST}:${PORT}`);
+  });
 }
 
 init();
